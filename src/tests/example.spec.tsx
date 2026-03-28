7f3f55986191ae2a78386038891d1777d88f6c9b
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2026-03-28 15:39:55 -0300

    test(sidebar): should render new prompt button

--- src/tests/example.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@/lib/test-utils';
-
-describe('Example', () => {
-  it('should be passed ', () => {
-    render(<div>John Doe</div>);
-
-    expect(screen.getByText('John Doe')).toBeInTheDocument();
-  });
-});
